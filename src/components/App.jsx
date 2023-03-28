bc0d897a49fc2d09a1fd579b4e265b2ac118bcbf
mgandolfi7 <michaelgandolfi7@gmail.com>
2023-03-28 15:45:52 -0500

    adjusted route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,8 +38,8 @@ useEffect(() => {
       <div>
         <Navbar user={user}/>
         <Routes>
-           <Route path="/" element={ user ? <Dashboard /> : <Navigate to='/login' /> } /> 
-           <Route path="/login" element={user ? <Navigate to='/' /> : <Login />} />
+           <Route path="/" element={ user ? <Navigate to='/dashboard' /> : <Login /> } /> 
+           <Route path="/dashboard" element={ <Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
