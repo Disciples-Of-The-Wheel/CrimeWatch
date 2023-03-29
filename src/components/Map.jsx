8cfbefb39885f119bbac146bd971fdeb40dcada1
mgandolfi7 <michaelgandolfi7@gmail.com>
2023-03-29 13:14:41 -0500

    add worker-loader

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl';
+import mapboxgl from 'mapbox-gl';
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 // mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
 
 const Map = ({ reports }) => {
