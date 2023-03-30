c4cc1847477bee7143506876580eaceb1485cd81
kevin <kklein166@gmail.com>
2023-03-30 11:42:13 -0500

    fix reports prop issue

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,15 +14,15 @@ export default function Map({ reports, zipcode }) {
   const [lng, setLng] = useState(-90.0715);
   const [lat, setLat] = useState(29.9511);
   const [zoom, setZoom] = useState(9);
-   
+
   useEffect(() => {
-  if (map.current) return; // initialize map only once
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [lng, lat],
-  zoom: zoom
-  });
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
   });
 
   useEffect(() => {
@@ -31,21 +31,23 @@ export default function Map({ reports, zipcode }) {
     if (reports === []) {
       return;
     } else {
-      for (let i = 0; i < reports.data.length; i++) {
-        new mapboxgl.Marker()
-        .setLngLat([reports.data[i].location.coordinates[0], reports.data[i].location.coordinates[1]])
-        .addTo(map.current);
+      if (reports && reports.data) {
+        for (let i = 0; i < reports.data.length; i++) {
+          new mapboxgl.Marker()
+            .setLngLat([reports.data[i].location.coordinates[0], reports.data[i].location.coordinates[1]])
+            .addTo(map.current);
+        }
       }
     }
-    })
+  })
 
   // useEffect(() => {
   //   console.log('zip', zipcode)
   // })
 
   return (
-  <div>
-  <div ref={mapContainer} className="map-container" />
-  </div>
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
   );
-  }
+}
