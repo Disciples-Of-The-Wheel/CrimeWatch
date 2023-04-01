a9632bad49a9593f228886ab72c2f153d9e1bd48
kevin <kklein166@gmail.com>
2023-03-31 19:07:36 -0500

    change timeline box data descriptions

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -11,10 +11,10 @@ const TimelineItem = ({ report }) => {
           {report.incident_type}
         </span>
         {/* <time>{report.timecreate}</time> */}
-        <p className="time-create">Date and Time Reported: {formattedTime}</p>
+        <p className="time-create">Date and Time: {formattedTime}</p>
         <p className='block-address'>Block Address: {report.address}</p>
         <p className="timeline-zip">Zip: {report.zip}</p>
-        <p className="disposition">Disposition: {report.disposition}</p>
+        <p className="disposition">Result: {report.disposition}</p>
         <span className="circle"></span>
       </div>
     </div>
